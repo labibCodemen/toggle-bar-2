c6cf5c02f6e40d31f19a4c12f412b904d9fd1b1c
labibCodemen <labibhassan016@gmail.com>
2021-10-07 12:54:20 +0600

    new commit

---
+++ project/main.js
@@ -0,0 +1,9 @@
+function myFunction(){
+    let circle = document.getElementById('circle');
+    let main = document.getElementById('main');
+    let box = document.getElementById('box')
+    main.classList.toggle('bg-green-500');
+    circle.classList.toggle('left-14');
+    box.classList.remove('hidden')
+
+}
